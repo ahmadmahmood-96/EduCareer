29fca66d1931dfae52e5471904539a21d7268b9a
Ayesha Qazi <qaziayesha966@gmail.com>
2024-04-25 14:52:24 +0500

    testing

--- Server/db.js
+++ Server/db.js
@@ -9,6 +9,7 @@ module.exports = async () => {
   try {
     await mongoose.connect(process.env.DB, connectionParams);
     console.log('Connected to Mongo DB');
+    console.log("");
   } catch (error) {
     console.log(error);
     console.log('Could not connect to Mongo DB');
